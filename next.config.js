7ecc3a69973f894cc600e6c3f558121935fc4b6d
waleed417 <khanwaleed13240@gmail.com>
2026-03-29 05:09:07 +0500

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: { unoptimized: true },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-}
-module.exports = nextConfig
